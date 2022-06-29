3d186a817b136fe2c1f09dbc891146ba596606ab
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-29 15:14:02 -0500

    Created pages and routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// PAGES
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Chat" element={<Chat/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
